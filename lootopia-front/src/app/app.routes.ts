1562be41af987ccae4ad5da6fbc53ee4c92b262f
anicsonrico <anicsonrico@gmail.com>
2025-04-17 14:30:02 +0200

    Home page & notFoundPage done

--- lootopia-front/src/app/app.routes.ts
+++ lootopia-front/src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { HomePageComponent } from './routes/home-page/home-page.component';
+import { NotFoundPageComponent } from './routes/not-found-page/not-found-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    title: 'Lootopia Home page',
+    component: HomePageComponent,
+  },
+  {
+    path: '**',
+    title: 'Lootopia not found page',
+    component: NotFoundPageComponent,
+  },
+];
